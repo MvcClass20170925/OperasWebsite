5b5d3e7f09f0dc409bd64136b4ceeaa483d45629
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-10-17 14:13:27 +0200

    Operas demo web site

---
+++ OperasWebSites/Models/Opera.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperasWebSites.Models
+{
+    public class Opera
+    {
+        public int OperaID { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        [CheckValidYear]
+        public int Year { get; set; }
+
+        public string Composer { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CheckValidYear : ValidationAttribute
+    {
+
+        public CheckValidYear()
+        {
+            ErrorMessage = "The earliest Opera is Daphne, 1598, by Corsi, Peri and Rinuccini.";
+        }
+
+        public override bool IsValid(object value)
+        {
+            int year = (int)value;
+            if( year < 1598 )
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
